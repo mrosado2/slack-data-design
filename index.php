2400d821baf3913a620d534bab84fa311631fb24
Maria Rosado <mrosado2@cnm.edu>
2016-10-13 09:57:34 -0600

    pushed updates to slack

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
 <!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<!-- add CSS file -->
+		<title>Data Design Slack Direct Message</title>
+	</head>
+	<body>
+		<!-- this is a header --><header>
+			<h1>Data Design Project</h1>
+		</header>
+
+		<!-- this is the main page content -->
+		<main>
+			<!-- begin section 1 -->
+			<section>
+				<h2>Direct Message</h2>
+				<h3>Group Directory</h3>
+				<h2>Persona</h2>
+				<p>Name: Valentina Bravo</p>
+				<p>Age: 37 years old</p>
+				<p>Profession: Furniture Designer</p>
+				<p>Hobbies: Hicking</p>
+				<p>Technology:Google phone, Mac </p>
+				<p>Goals: Establish a stable business and hick as much as possible</p>
+			</section>
+			<!-- begin section 2 -->
+			<section>
+				<h2>Use Case</h2>
+				<ul>
+					<li>private messages</li>
+					<li>conversations</li>
+					<li>document  sharing</li>
+				</ul>
+			</section>
+			<!--begin section 3 -->
+			<section>
+				<h2>Entities</h2>
+				<p>Directory</p>
+				<p>Find or start a conversation field</p>
+				<p>Recent conversations</p>
+			</section>
+		</main>
+	</body>
+</html>
