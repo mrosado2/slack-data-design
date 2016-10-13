b47cad7cd210e443b8159bce60e6f6d3c4fb35b7
Maria Rosado <mrosado2@cnm.edu>
2016-10-13 16:28:30 -0600

    pushed update to persona

--- index.php
+++ index.php
@@ -7,30 +7,34 @@
 	</head>
 	<body>
 		<!-- this is a header --><header>
-			<h1>Data Design Project</h1>
+			<h3>Data Design Slack Messaget</h3>
 		</header>
-
-		<!-- this is the main page content -->
 		<main>
 			<!-- begin section 1 -->
 			<section>
-				<h2>Direct Message</h2>
-				<h3>Group Directory</h3>
-				<h2>Persona</h2>
-				<p>Name: Valentina Bravo</p>
-				<p>Age: 37 years old</p>
-				<p>Profession: Furniture Designer</p>
-				<p>Hobbies: Hicking</p>
-				<p>Technology:Google phone, Mac </p>
-				<p>Goals: Establish a stable business and hick as much as possible</p>
+				<h4>Persona</h4>
+				<ul>
+					<li>Name: Gloria Mendez </li>
+					<li>Age:27</li>
+					<li>Profession: Acconting Student</li>
+					<li>Changing carrer from Elementary school Math Teacher.</li>
+					<li>Hobbies: Blogging and Fotografy</li>
+					<li>Immediate plan: She just decided to go back to school to complete her Accounting studies.
+					In plans to start her own business at home in partnership with her sister.
+						She wants to do this as she is plannig on being able to set
+					her own work schedule to have more time for fotography and blogging </li>
+				<li>Goals: Establish her business to manage her time according to her passions</li>
+				</ul>
 			</section>
 			<!-- begin section 2 -->
 			<section>
 				<h2>Use Case</h2>
 				<ul>
-					<li>private messages</li>
-					<li>conversations</li>
-					<li>document  sharing</li>
+					<li>Use Slack messages to manage in one site her communication with the groups of her interest.
+					</li>
+					<li>Share documents to work with her sister, maintaining a fast and effective communication and
+						colaboration,even when she is at school.
+						And possibly later will create her own teams as when she needs help and create her own groups</li>
 				</ul>
 			</section>
 			<!--begin section 3 -->
