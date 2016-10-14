f304b69dc4fca8094a833d1bbdfe756bac944025
Maria Rosado <mrosado2@cnm.edu>
2016-10-13 23:52:49 -0600

    pushed changer on the user

--- index.php
+++ index.php
@@ -5,51 +5,55 @@
 		<title>Slack Message Data Design Project</title>
 	</head>
 	<body>
-		<<header>
-			<h3>Data Design Slack Messaget</h3>
+		<header>
+				<main>
+			<h1>Data Design Slack Message</h1>
 		</header>
-		<main>
-			<section>
-				<h4>User</h4>
+		<main><h3>User</h3>
 				<p>
 					Gloria will be using Slack direct messanger with her school peers for ongoing project colaboration.
 				</p>
 				<ul>
-					<li>Name: Gloria Mendez</li>
-					<li>Age:27</li>
-					<li>Hobbies: Photography and hiking</li>
-					<li>Profession: Photography Student</li>
-					<li>Back to school to continue her Art studies sinced she wants to change careers from Highschool Art Teacher.
-						Gloria wants to specialize in Photography, to start her own business and to be able to work her own schedule,
-						enjoy nature and to have the freedom to express her own style in her work. She will partner with
-						her sister to have someone reliable and supportive on her business. Gloria is planning to create her
-						team when she completed school and starts her business, to maintain consistency on her files and save
-						time since she only will need to check one app.
-						Gloria and her sister will use their Android phones and
-					their Mac computers at home and work.</li>
-					<li>Technology: Gloria will be using her Android phone and her Mac computer at home and work </li>
+					<li><strong>Name:</strong> Gloria Mendez</li>
+					<li><strong>Age:</strong>27</li>
+					<li><strong>Hobbies:</strong> Photography and hiking</li>
+					<li><strong>Profession:</strong> Photography Student</li>
+					<li><strong>Technology></strong>Gloria will be using her Android phone and her Mac computer at home and school.</li>
 				</ul>
-			<section>
-				<h4>Use Case</h4>
-				<p><strong>Goal</strong> Gloria was introduce to Slack at her class in CNM since it had been very effective
-					to communicate and colaborate with her team. Later she wants to use it for her business.</p>
+					<p>Gloria decided to go back to school to continue her Photography studies sinced she wants to change careers from
+						High school Art Teacher, to professional photographer and start her own business, as she wants to be able to work her
+						own schedule to enjoy nature and to have the freedom to express her own style in her work.</p>
+						<p>She wants to make the most out of the time in school having the opportunity to learn as much as possible from
+							the experience the professors and her peers to become an excellent photographer</p>
+						<p>Gloria wants to start her busimess in partnership with her sister since they work well together and combining their
+						talents on their company they will have more band opportunities to succeed. Gloria is contemplating the possibility
+						to use the same app for her business by creating a group in Slack for her and her sister.</p>
+				<h3>Use Case</h3>
+				<p><strong>Goal:</strong>Gloria wants to become very effective at her communication with Slack. She has commited
+					to use it as much as possible while in school to ensure the use of this app will be convinient for her business.</p>
+			<ol>
+				<li>Gloria will download the app to her phone and computer</li>
+				<li>Gloria will register and will receive her userId</li>
+				<li>Gloria will log in and will join her school team</li>
+				<li>Gloria will contact group members to ask for feedback on her work</li>
+			</ol>
+			<h2>Conceptual Model</h2>
+			<h3>Entities and Atributes</h3>
+			<h2>user</h2>
 				<ul>
-					<li>Gloria will .
-					</li>
-					<li>She likes uses the message to also share documents to work with her sister, maintaining a fast and
-						effective communication and
-						colaboration,even when she is at school.
-						And possibly later will create her own teams</li>
+					<li>userId (primary key)</li>
+					<li>userName</li>
+					<li>userPicture</li>
 				</ul>
-			</section>
-			<!--begin section 3 -->
-			<section>
-				<h4>Entities</h4>
-				<ul>
-					<li>User</li>
-					<li>message</li>
-				</ul>
-			</section>
+			<h2>message</h2>
+			<ul>
+				<li>messageId (primary key)</li>
+				<li>messageTime</li>
+				<li>messageContent</li>
+			</ul>
+			<h3>Relationships</h3>
+			<p>user can send many messages 1-to-n </p>
+			<p>user can receive many messages m-to-</p>
 		</main>
 	</body>
 </html>
