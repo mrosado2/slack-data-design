05609c0e94e3877a1b7c6f6313c162b4ee5579b6
Maria Rosado <mrosado2@cnm.edu>
2016-10-16 22:32:09 -0600

    push updates

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		</header>
 		<main><h3>User</h3>
 				<p>
-					Gloria will be using Slack Direct Messange with her school peers and professors for ongoing project
+					Gloria will be using Slack Direct Message with her school peers and professors for ongoing project
 					colaboration.
 				</p>
 				<ul>
@@ -23,26 +23,26 @@
 						school.</li>
 				</ul>
 					<p>Gloria decided to go back to school to continue her Photography studies sinced she wants to
-						change careers fromHigh school Art Teacher, to professional photographer and start her
-						own business, as she wants to be able to work her own schedule to enjoy nature and to have
+						change careers from High school Art Teacher, to professional photographer and start her
+						own business.
+						She wants to be her own boss and be able to work her own schedule, enjoy nature and to have
 						the freedom to express her ownstyle in her work.</p>
-						<p>She wants to make the most out of the time in school to learn as much as possible from
-							the experience of the professors and her peers, as she wants to become an excellent
-							photographer. </p>
+						<p>She wants to make the most out of her time in school and learn from the experience and technique
+							of the professors and her peers, as she wants to become an excellent photographer. </p>
 						<p>Gloria wants to start her business in partnership with her sister since they work well
-							together and by combining their talents on the company they will give more
-							opportunities to succeed.
-							Gloria is contemplating the possibility to use the same app to cummunicate with her
-							sister and other collaborators for her business, by creating her own team in Slack.</p>
+							together and by combining their talents on the company they will have better opportunities
+							to succeed.
+							Gloria is contemplating the possibility also implement the use of Slack for her business
+							to cummunicate with her sister and her employees by creating her own team in Slack.</p>
 				<h3>Use Case</h3>
 				<p><strong>Goal:</strong> Gloria wants to become very effective at her communication with Slack.
 					She has commited herself to use it as much as possible to maximaze her learning while in
-					school and to ensure that theuse of this app will be convinient for her business.</p>
+					school and to ensure that the use of this app will be convinient for her business.</p>
 			<ol>
 				<li>Gloria will download the app to her phone and computer</li>
 				<li>Gloria will register and will receive her userId</li>
 				<li>Gloria will log in and will join her school team</li>
-				<li>Gloria will contact group members to ask for feedback on her work</li>
+				<li>Gloria will contact group members to ask for feedback on her work and plan practice field trips.</li>
 			</ol>
 			<h3>Conceptual Model</h3>
 			<h3>Entities and Atributes</h3>
@@ -57,13 +57,15 @@
 				<li>messageId (primary key)</li>
 				<li>messageTime</li>
 				<li>messageContent</li>
-				<li>messagePosterId  foreign key</li>
+				<li>messageSenderId  foreign key</li>
+				<li>messageReceiverId foreign key</li>
 			</ul>
 			<h4>Relationships</h4>
 			<p>userId can send many messages 1-to-n </p>
 			<p>userId can receive many messages 1-to-n</p>
 			<p>one message can be send to many usersIds m-to-n</p>
 			<p>messageId can get only 1-to-1 messageTimeStamp</p>
+			<p>message cab be sent to multiple receivers 1-to-n</p>
 		</main>
 	</body>
 </html>
