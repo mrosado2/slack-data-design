10be6e8b30ed0414ca66422e94fa3b91210bf1e1
Maria Rosado <mrosado2@cnm.edu>
2016-10-20 11:59:09 -0600

    accessor and mutator for user

--- class/Profile.php
+++ class/Profile.php
@@ -33,8 +33,54 @@ private $senderProfileId;
  * @return int/null value of user id
  **/
 public function getUserId() {
-	return($this->userId);
-	
 
+	return($this->userId);
 }
+/**
+ * mutator method for user id
+ *
+ * @param int/null $newUserId new value of user id
+ * @throws \RangeException if $newUserId is not positive
+ * @throws \TypeError if $newUserId is not an integer
+ **/
+	public function setUserId(int $newUserId = null) {
+		// base case: if the user id is null, this a new user without a mySQL assigned id (yet)
+		if($newUserId === null){
+			$this->userId = null;
+			return;
+		}
+		// verify the user id is positive
+		if($newUserId <= 0){
+			throw(new \RangeException("user id is not positive"));
+		}
+
+		// convert and store the user id
+		$this->userId = $newUserId;
+	}
+
+	/**
+	 * accessor method for user profile id
+	 *
+	 * @return int value of user profile id
+	 **/
+	public function getUserProfileId() {
+		 return($this->userProfileId);
+	}
+
+	/**
+	 * mutator method for user profile id
+	 *
+	 * @param int $newUserProfileId new value of user profile id
+	 * @throws \RangeException if $newProfileId is not positive
+	 * @throws \TypeError if $newProfileId is not an integer
+	 **/
+	public function setUserProfileId(int $newUserProfileId) {
+			// verify the profile id is positive
+		if($newUserProfileId <= 0) {
+			throw(new \RangeException("user profile id is not positive"));
+		}
+
+		// convert and store the profile id $this->userProfileId = $newUserProfileId;
+		}
+	}
 }
